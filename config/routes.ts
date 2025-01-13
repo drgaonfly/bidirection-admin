4a2f755269e7cf54a28f08cbab198747a5e89654
15767956229@163.com <15767956229@163.com>
2025-01-13 14:44:09 +0800

    fix: Translate new

--- config/routes.ts
+++ config/routes.ts
@@ -89,12 +89,12 @@ export default [
     ],
   },
   {
-
     path: '/dashboard',
     name: 'list.dashboard',
     icon: 'DashboardOutlined',
     component: './Dashboard',
   },
+  {
     path: '/multilingual-management',
     name: 'list.multilingual-management',
     icon: 'TranslationOutlined',
@@ -103,6 +103,12 @@ export default [
         path: '/multilingual-management/languages',
         name: 'list.languages',
         component: './MultilingualManagement/Langue',
+        icon: 'TranslationOutlined',
+      },
+      {
+        path: '/multilingual-management/translates',
+        name: 'list.translates',
+        component: './MultilingualManagement/Translate',
         icon: 'TranslationOutlined',
       },
     ],
