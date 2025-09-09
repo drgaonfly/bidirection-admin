bc2f41c54d58c28331f391b476520cd326343b2f
dragonfly <2237676323@qq.com>
2025-09-09 18:39:18 +0800

    refactor: remove isRecycled badge from EnergyUsage table

--- src/pages/Energy/EnergyUsage/index.tsx
+++ src/pages/Energy/EnergyUsage/index.tsx
@@ -3,7 +3,7 @@ import { queryList, removeItem } from '@/services/ant-design-pro/api';
 import type { ActionType, ProColumns, ProDescriptionsItemProps } from '@ant-design/pro-components';
 import { FooterToolbar, PageContainer, ProTable } from '@ant-design/pro-components';
 import { FormattedMessage, useAccess } from '@umijs/max';
-import { Badge, message } from 'antd';
+import { message } from 'antd';
 import React, { useRef, useState } from 'react';
 import Show from './components/Show';
 import DeleteButton from '@/components/DeleteButton';
@@ -104,17 +104,6 @@ const TableList: React.FC = () => {
       title: intl.formatMessage({ id: 'pens', defaultMessage: '笔数' }),
       dataIndex: 'pens',
       hideInSearch: true,
-    },
-    {
-      title: intl.formatMessage({ id: 'isRecycled', defaultMessage: '是否回收' }),
-      dataIndex: 'isRecycled',
-      renderText: (_, record) => {
-        return record?.isRecycled ? (
-          <Badge status="success" text="已回收" />
-        ) : (
-          <Badge status="error" text="未回收" />
-        );
-      },
     },
     {
       title: intl.formatMessage({ id: 'tx_id', defaultMessage: '交易哈希' }),
